613d3a012c68c0f96144e041e3d0b7c60f1dbffe
Ridwan <officiallyrid1@gmail.com>
2023-04-07 01:34:45 -0400

    Trying to commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Pup fluffer = new Pup("fluffer", 4, "Husky");
+
+        System.out.println(fluffer.breed);
+    }
+}
+ 
---
+++ src/Pup.java
@@ -0,0 +1,11 @@
+
+public class Pup {
+    String name, breed;
+    int age;
+    Pup(String name, int age, String breed){
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+}
+
