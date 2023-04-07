c006aed0a1c49c517df09ac93dbb0ba2eec77af4
Ridwan <officiallyrid1@gmail.com>
2023-04-06 23:59:15 -0400

    defined some constructors for dog

---
+++ src/Dog.java
@@ -0,0 +1,10 @@
+
+public class Dog {
+    String name, breed;
+    int age;
+    Dog(String name, int age, String breed){
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Dog fluffer = new Dog("fluffer", 4, "Husky");
+
+        System.out.println(Dog.name);
+    }
+}
+ 
