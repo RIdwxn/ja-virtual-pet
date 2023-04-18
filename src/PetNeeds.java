571c52d05c9f13641406fcc6fe37054aebd0f8c4
Ridwan~ <officiallyrid1@gmail.com>
2023-04-18 17:36:43 -0400

    Getters and Setters

---
+++ src/PetNeeds.java
@@ -0,0 +1,78 @@
+public class PetNeeds {
+
+    private String name;
+    private int hunger;
+    private int thirst;
+    private int bladderLevel;
+    private int loneliness;
+    private int exhaustion;
+    private int dirtiness;
+
+    public PetNeeds(){
+            
+    }
+
+    public void setHunger(int hungerLevel){
+        ///setter
+        hunger = hungerLevel;
+        
+    }
+    public int getHunger(){
+        return hunger;
+    }
+
+    public void setDirtiness(int dirtinessLevel){
+
+    }
+
+    public int getDirtiness(){
+        return dirtiness;
+    }
+
+    public void setLoneliness(int lonelinessLevel){
+
+    }
+
+    public int getLoneliness(){
+        return loneliness;
+    }
+
+    public void setExhaustion(int exhautionLevel){
+
+    }
+
+    public int getExhaustion(){
+        return exhaustion;
+    }
+
+    public void thirst(int thirstLevel){
+
+    }
+
+    public int getThirst(){
+        return thirst;
+    }
+
+    public void setBladder(int bladderLevel){
+
+    }
+
+    public int getBladder(){
+        return bladderLevel;
+    }
+
+    public void setName(String newName){
+        ///setter
+        name = newName;
+
+    }
+    //getter 
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        
+        
+    }
+}
